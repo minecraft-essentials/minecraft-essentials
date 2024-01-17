80fc3e7bd78cfa54a4b4f2025656956797e59f93
eveeify <eveeg1971@gmail.com>
2024-01-18 00:17:07 +1100

    chore: added licencing fix: grammar

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,24 @@
+/* Minecraft-Essentials
+ * Copyright (C) 2024 minecraft-essentials
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program.
+ */
+
 #![forbid(unsafe_code, missing_docs)]
 #![warn(clippy::pedantic)]
 
-//! The server for OAauth login Technically
+//! The server for OAauth login Server.
 
 use actix_web::{web, App, HttpResponse, HttpServer};
 use serde::Deserialize;
