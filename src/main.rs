f533d4dc6ca40a56ae92a4aa5ceed7c6da034bda
eveeify <eveeg1971@gmail.com>
2024-02-29 23:18:01 +1100

    fix: cli help

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use clap::{Command, Parser, Subcommand};
-use minecraft_essentials::{CustomAuthData, DeviceCode, Oauth};
+use clap::{Args, Parser, Subcommand};
+use minecraft_essentials::{DeviceCode, Oauth};
 
 #[derive(Parser)]
-#[command(version = env!("CARGO_PKG_VERSION"), about = "A Cli for a minecraft client essentials library", long_about = None)]
-#[command(propagate_version = true)]
+#[command(version, long_about = None)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -12,38 +11,50 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Version {},
-    Oauth {client_id: String, client_secret: String, port: Option<u16>, bedrockrelm: Option<bool>},
-    DeviceCode {client_id: String, bedrockrelm: bool}
+    /// Oauth Check command.
+    Oauth(OauthArgs),
+    /// DeviceCode Check command.
+    DeviceCode(DeviceCodeArgs)
 }
 
+#[derive(Args)]
+struct OauthArgs {
+    client_id: String, 
+    client_secret: String, 
+    port: Option<u16>, 
+    bedrockrelm: Option<bool>
+}
 
-#[tokio::main] // Assuming you're using Tokio as your async runtime
+#[derive(Args)]
+struct DeviceCodeArgs {
+    client_id: String, 
+    bedrockrelm: bool
+}
+
+#[tokio::main]
 async fn main() {
     let cli = Cli::parse();
     match &cli.command {
-        Commands::Oauth { client_id, client_secret, port, bedrockrelm } => {
-    
-            let auth = Oauth::new(&client_id, Some(port));
+        Commands::Oauth(oauth_args) => handle_oauth(oauth_args).await,
+        Commands::DeviceCode(device_code_args) => handle_device_code(device_code_args).await,
+        Commands::Version {} => println!("{}", env!("CARGO_PKG_VERSION")),
+    }
+}
 
-            println!("Your Authentification URL is Here: {}", auth.url());
-
-            println!("Waiting for Login........");
-            let authinfo: CustomAuthData = auth
-                .launch(Option<*bedrockrelm>, &client_secret)
-                .await
-                .expect("Failed to launch");
-
+async fn handle_oauth(oauth_args: &OauthArgs) {
+    let auth = Oauth::new(&oauth_args.client_id, Some(oauth_args.port.unwrap_or(8000)));
+    println!("URL: {} \nWaiting for Login........", auth.url());
+    match auth.launch(oauth_args.bedrockrelm.unwrap_or(false), &oauth_args.client_secret).await {
+        Ok(authinfo) => {
             println!(
                 "Bearer: {:?}, \n UUID: {:?}, \n Expire_in: {:?}, \n XtsToken: {:?}",
                 authinfo.access_token, authinfo.uuid, authinfo.uuid, authinfo.xts_token
             );
         },
-        Commands::DeviceCode { client_id, bedrockrelm } => {
-
-        },
-        Commands::Version {} => {
-            println!("{}", env!("CARGO_PKG_VERSION"))
-        },
-        _ => unreachable!(),
+        Err(e) => eprintln!("Failed to launch: {}", e),
     }
+}
+
+async fn handle_device_code(device_code_args: &DeviceCodeArgs) {
+    // Implement the logic for handling DeviceCodeArgs here
 }
