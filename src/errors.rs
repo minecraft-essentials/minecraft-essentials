690413a4ba7eded19c4020c13882cba33da6ae7f
eveeifyeve <eveeg1971@gmail.com>
2024-06-15 08:34:01 +1000

    chore: cleaned up errors

--- src/errors.rs
+++ src/errors.rs
@@ -3,30 +3,10 @@
 
 use displaydoc::Display;
 use thiserror::Error;
-/// The `TokenError` enum represents potential errors that can occur during token operations.
-#[derive(Display, Error, Debug)]
-pub enum TokenError {
-    /// Response Failed: {0}
-    ResponseError(String),
-}
 
-/// The `XboxError` enum represents potential errors that can occur during Xbox-related operations.
+/// The `AuthErrors` represents potential errors that can occur during Authentication.
 #[derive(Display, Error, Debug)]
-pub enum XboxError {
-    /// Response Failed: {0}
-    ResponseError(String),
-}
-
-/// The `XTSError` enum represents potential errors that can occur during XTS-related operations.
-#[derive(Display, Error, Debug)]
-pub enum XTSError {
-    /// Response Failed: {0}
-    ResponseError(String),
-}
-
-/// The `OAuthError` enum represents potential errors that can occur during OAuth authentication.
-#[derive(Display, Error, Debug)]
-pub enum OAuthError {
+pub enum AuthErrors {
     /// Authentcation Failed: {0}
     AuthenticationFailure(String),
     /// Parsing Failed: {0}
@@ -41,11 +21,35 @@ pub enum OAuthError {
     ParseInfoError(String),
     /// Failed to accept connection: {0}
     AcceptConnectionError(String),
+    /// Response Failed: {0}
+    ResponseError(String),
 }
 
-/// The `LaunchError` enum represents potential errors that can occur during Launching minecraft.
+/// The `LaunchErrors` enum represents potential errors that can occur during Launch-related operations.
 #[derive(Display, Error, Debug)]
-pub enum LaunchError {
-    /// Launch Requirements Failed: {0}
+pub enum LaunchErrors {
+    /// Unsupported Device: {0}
+    UnsupportedDevice(String),
+    /// Failed to fetch username {0}
+    UsernameFetchError(String),
+    /// Unsuported Archtechure: {0} switch to another cpu archtechure and try again.
+    UnsupportedArchitecture(String),
+    /// Response Failed: {0}
+    ResponseError(String),
+    /// Requirements not reached: {0}
     Requirements(String),
+}
+
+/// The `XboxError` enum represents potential errors that can occur during Xbox-related operations.
+#[derive(Display, Error, Debug)]
+pub enum XboxError {
+    /// Response Failed: {0}
+    ResponseError(String),
+}
+
+/// The `XTSError` enum represents potential errors that can occur during XTS-related operations.
+#[derive(Display, Error, Debug)]
+pub enum XTSError {
+    /// Response Failed: {0}
+    ResponseError(String),
 }
