edbe368a99addf2bde57f7cf81553b227850afb9
eveeifyeve <eveeg1971@gmail.com>
2024-06-15 12:36:33 +1000

    fix: cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Args, Parser, Subcommand};
-use minecraft_essentials::{AuthType, AuthenticationBuilder, Launch, Oauth};
+use minecraft_essentials::{AuthType, AuthenticationBuilder};
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -16,8 +16,8 @@ enum Commands {
     Oauth(OauthArgs),
     /// DeviceCode Check command.
     DeviceCode(DeviceCodeArgs),
-    /// Custom Launch Check command
-    CustomLaunch(CustomLaunchArgs),
+    // / Custom Launch Check command
+    // CustomLaunch(CustomLaunchArgs),
 }
 
 #[derive(Args)]
@@ -54,47 +54,63 @@ async fn main() {
         Commands::Oauth(oauth_args) => handle_oauth(oauth_args).await,
         Commands::DeviceCode(device_code_args) => handle_device_code(device_code_args).await,
         Commands::Version {} => println!("{}", env!("CARGO_PKG_VERSION")),
-        Commands::CustomLaunch(handle_custom_launch_args) => {
-            handle_custom_launch(handle_custom_launch_args).await
-        }
+        // Commands::CustomLaunch(handle_custom_launch_args) => {
+        //     handle_custom_launch(handle_custom_launch_args).await
+        // }
     }
 }
 
 async fn handle_oauth(oauth_args: &OauthArgs) {
-    let auth = Oauth::new(&oauth_args.client_id, Some(oauth_args.port.unwrap_or(8000)));
-    println!("URL: {} \nWaiting for Login........", auth.url());
-    let auth_info = auth.launch(false, &oauth_args.client_secret).await.unwrap();
-    println!(
-        "Bearer: {:?}, \n UUID: {:?}, \n Expire_in: {:?}, \n XtsToken: {:?}",
-        auth_info.access_token, auth_info.uuid, auth_info.expires_in, auth_info.xts_token
-    );
+    let mut auth_builder = AuthenticationBuilder::builder();
+    auth_builder
+        .of_type(AuthType::Oauth)
+        .await
+        .client_id(&oauth_args.client_id)
+        .client_secret(&oauth_args.client_secret)
+        .bedrockrel(oauth_args.bedrockrelm)
+        .port(oauth_args.port);
+
+    println!("{:?}", auth_builder.get_info());
+
+    let auth_info = auth_builder.launch().await.unwrap();
+
+    println!("{:?}", auth_info)
 }
 
-async fn handle_device_code(_device_code_args: &DeviceCodeArgs) {
-    println!("{}", EXPERIMENTAL_MESSAGE);
+async fn handle_device_code(device_code_args: &DeviceCodeArgs) {
+    let mut auth_builder = AuthenticationBuilder::builder();
+    auth_builder
+        .of_type(AuthType::DeviceCode)
+        .await
+        .client_id(&device_code_args.client_id)
+        .bedrockrel(Some(device_code_args.bedrockrelm));
+
+    println!("{:?}", auth_builder.get_info());
+
+    println!("{:?}", auth_builder.launch().await);
 }
 
-async fn handle_custom_launch(handle_custom_launch_args: &CustomLaunchArgs) {
-    let mut args = Vec::new();
+// async fn handle_custom_launch(handle_custom_launch_args: &CustomLaunchArgs) {
+//     let mut args = Vec::new();
 
-    args.push(format!("--token{}", handle_custom_launch_args.token));
-    args.push(format!("--uuid{}", handle_custom_launch_args.uuid));
+//     args.push(format!("--token{}", handle_custom_launch_args.token));
+//     args.push(format!("--uuid{}", handle_custom_launch_args.uuid));
 
-    if !handle_custom_launch_args.optional_args.is_empty() {
-        args.push(handle_custom_launch_args.optional_args.clone())
-    }
+//     if !handle_custom_launch_args.optional_args.is_empty() {
+//         args.push(handle_custom_launch_args.optional_args.clone())
+//     }
 
-    let launch = Launch::new(
-        args,
-        handle_custom_launch_args.java_exe.clone(),
-        handle_custom_launch_args.jrepath.clone(),
-        handle_custom_launch_args.offline,
-    )
-    .expect("Expected Launch");
+//     let launch = Launch::new(
+//         args,
+//         handle_custom_launch_args.java_exe.clone(),
+//         handle_custom_launch_args.jrepath.clone(),
+//         handle_custom_launch_args.offline,
+//     )
+//     .expect("Expected Launch");
 
-    let launch_info = launch.info();
+//     let launch_info = launch.info();
 
-    println!("Launching with: {:?}", launch_info);
+//     println!("Launching with: {:?}", launch_info);
 
-    let _ = launch.launch_jre();
-}
+//     let _ = launch.launch_jre();
+// }
