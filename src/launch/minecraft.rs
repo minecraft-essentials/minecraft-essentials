a268bc7052c35ca945907708743dd3eb1a09235b
eveeifyeve <eveeg1971@gmail.com>
2024-06-25 22:42:12 +1000

    feat: manifest & version manifest working

--- src/launch/minecraft.rs
+++ src/launch/minecraft.rs
@@ -1,16 +1,14 @@
-use crate::structs::VersionManifest;
+use crate::{
+    structs::{ManifestVersion, VersionManifest},
+    MANIFEST_URL,
+};
 use reqwest::{header::USER_AGENT, Client};
-use serde::Deserialize;
 
-#[derive(Deserialize)]
-pub struct ManifestVersion {
-    pub latest: Latest,
-}
-
-#[derive(Deserialize)]
-pub struct Latest {}
-
-pub async fn get_version_manifest(client: &Client, url: &str, user_agent: &str) {
+pub async fn get_version_manifest(
+    url: &str,
+    user_agent: &str,
+) -> Result<VersionManifest, Box<dyn std::error::Error>> {
+    let client = Client::new();
     let result = client
         .get(url)
         .header(USER_AGENT, user_agent)
@@ -18,4 +16,20 @@ pub async fn get_version_manifest(client: &Client, url: &str, user_agent: &str) 
         .await?;
 
     let version_manifest: VersionManifest = result.json().await?;
+
+    Ok(version_manifest)
+}
+
+pub async fn get_manifest(user_agent: &str) -> Result<ManifestVersion, Box<dyn std::error::Error>> {
+    let client = Client::new();
+
+    let result = client
+        .get(MANIFEST_URL)
+        .header(USER_AGENT, user_agent)
+        .send()
+        .await?;
+
+    let manifest: ManifestVersion = result.json().await?;
+
+    Ok(manifest)
 }
