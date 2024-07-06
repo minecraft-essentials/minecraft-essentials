a4734133dab1dab5286e6cee6a42fccb2ad0feaf
eveeifyeve <eveeg1971@gmail.com>
2024-07-06 19:06:41 +1000

    fix: tests

--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ use super::*;
 use dotenv::dotenv;
 use std::env;
 
-#[cfg(feature = "custom-auth")]
+#[cfg(feature = "auth")]
 #[tokio::test]
 async fn test_oauth_url() {
     let _ = dotenv();
@@ -16,7 +16,7 @@ async fn test_oauth_url() {
     assert_eq!(oauth.url(), expected_url);
 }
 
-#[cfg(feature = "custom-auth")]
+#[cfg(feature = "auth")]
 #[tokio::test]
 async fn test_device_code_prelaunch() {
     let _ = dotenv();
@@ -31,22 +31,75 @@ async fn test_device_code_prelaunch() {
     assert_eq!(user_code, device_code.user_code);
 }
 
-#[cfg(feature = "custom-launch")]
+#[cfg(feature = "auth")]
 #[tokio::test]
-async fn test_custom_launch() {
+async fn test_authentication_info() {
+    // Setup for OAuth
     let _ = dotenv();
-    let args = vec![
-        "--uuid:uuidtest".to_string(),
-        "--token:tokentest".to_string(),
-    ];
-    let args_join = args.join(" ");
-    let jre = Some(PathBuf::from("/test/java"));
-    let java_exe = "/test/java";
+    let client_id = env::var("Client_ID").expect("Expected Client ID.");
+    let client_secret = env::var("Client_Secret").expect("Expected Client Secret.");
+    let port = 8000;
+    let mut builder = AuthenticationBuilder::builder();
+    builder
+        .of_type(AuthType::Oauth)
+        .client_id(&client_id)
+        .client_secret(&client_secret)
+        .port(Some(port));
 
-    let launch =
-        Launch::new(args, java_exe.to_string(), jre.clone(), Some(false)).expect("Expected Launch");
-    let (launch_args, launch_java_exe, launch_jre) = launch.info();
-    assert_eq!(args_join, launch_args);
-    assert_eq!(java_exe, launch_java_exe);
-    assert_eq!(jre, launch_jre.clone());
+    let assert_url = format!("https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize/?clientid={}&response_type=code&redirect_uri=http://localhost:{}&response_mode=query&scope={}&state=12345", client_id, port, SCOPE);
+    let url = builder.get_info().await._ouath_url.unwrap(); // Note: There seems to be a typo in the method name. It should likely be something like get_oauth_url()
+
+    assert_eq!(assert_url, url);
+
+    builder.of_type(AuthType::DeviceCode).client_id(&client_id);
+
+    let device_code = builder.get_info().await._device_code.unwrap();
+    let url = device_code.verification_uri.clone();
+    let message = device_code.message.clone();
+    let expires_in = device_code.expires_in.clone();
+    let user_code = device_code.user_code.clone();
+
+    assert_eq!(url, device_code.verification_uri);
+    assert_eq!(message, device_code.message);
+    assert_eq!(expires_in, device_code.expires_in);
+    assert_eq!(user_code, device_code.user_code);
+}
+
+#[cfg(feature = "auth")]
+#[tokio::test]
+async fn test_authentication() {
+    let _ = dotenv();
+    let client_id = env::var("Client_ID").expect("Expected Client ID.");
+    let client_secret = env::var("Client_Secret").expect("Expected Client Secret.");
+    let port = env::var("Port").expect("Expected Port.");
+    let mut builder = AuthenticationBuilder::builder();
+    builder
+        .of_type(AuthType::Oauth)
+        .client_id(&client_id)
+        .client_secret(&client_secret)
+        .port(Some(port.parse::<u16>().unwrap()));
+
+    let auth_info = builder.launch().await.unwrap();
+    let access_token = auth_info.access_token.unwrap();
+    let uuid = auth_info.uuid.unwrap();
+    let expires_in = auth_info.expires_in;
+    let xts_token = auth_info.xts_token.unwrap();
+
+    assert_eq!(access_token.len(), 36);
+    assert_eq!(uuid.len(), 36);
+    assert!(expires_in > 0);
+    assert_eq!(xts_token.len(), 36);
+
+    builder.of_type(AuthType::DeviceCode).client_id(&client_id);
+
+    let auth_info = builder.launch().await.unwrap();
+    let access_token = auth_info.access_token.unwrap();
+    let uuid = auth_info.uuid.unwrap();
+    let expires_in = auth_info.expires_in;
+    let xts_token = auth_info.xts_token.unwrap();
+
+    assert_eq!(access_token.len(), 36);
+    assert_eq!(uuid.len(), 36);
+    assert!(expires_in > 0);
+    assert_eq!(xts_token.len(), 36);
 }
