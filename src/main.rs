cdabdf2597ac238072f4c73fd45d2fa97539d267
eveeifyeve <eveeg1971@gmail.com>
2024-07-06 18:15:12 +1000

    chore: cleaned up inputs for cli

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use std::path::PathBuf;
 
 use clap::{Args, Parser, Subcommand};
-use minecraft_essentials::{
-    structs::QuickPlayArguments, Args as MinecraftArgs, AuthType, AuthenticationBuilder,
-    LaunchArgs, LaunchArgsAuth, LaunchBuilder,
-};
+use minecraft_essentials::{AuthType, AuthenticationBuilder, LaunchBuilder};
 
 #[derive(Parser)]
 #[command(version, long_about = None)]
@@ -35,10 +32,11 @@ struct OauthArgs {
 #[derive(Args)]
 struct LaucnhArgs {
     // Java Args
-    min_memory: Option<i32>,
-    max_memory: i32,
+    min_memory: usize,
+    max_memory: Option<usize>,
     launcher_name: Option<String>,
     launcher_version: Option<String>,
+    jre: Option<JavaJRE>,
     class_path: Option<String>,
 
     // Game Args
@@ -47,8 +45,8 @@ struct LaucnhArgs {
     version: Option<String>,
     uuid: Option<String>,
     game_directory: Option<PathBuf>,
-    width: Option<u32>,
-    height: Option<u32>,
+    width: Option<usize>,
+    height: Option<usize>,
     access_token: Option<String>,
 
     // Quick Play Args
@@ -69,7 +67,8 @@ async fn main() {
         Commands::Oauth(oauth_args) => handle_oauth(oauth_args).await,
         Commands::DeviceCode(device_code_args) => handle_device_code(device_code_args).await,
         Commands::Version {} => println!("{}", env!("CARGO_PKG_VERSION")),
-        Commands::Launch(arg) => handle_launch(arg).await,
+        // TODO: HANDLE LAUNCH
+        Commands::Launch(arg) => todo!(),
     }
 }
 
@@ -101,29 +100,9 @@ async fn handle_device_code(device_code_args: &DeviceCodeArgs) {
     println!("{:?}", auth_builder.launch().await);
 }
 
-async fn handle_launch(arg: &LaucnhArgs) {
-    let quick_play_arguments = if let Some(singleplayer) = arg.quick_play_singleplayer.clone() {
-        QuickPlayArguments::SinglePlayer(singleplayer)
-    } else if let Some(multiplayer) = arg.quick_play_multiplayer.clone() {
-        QuickPlayArguments::MultiPlayer(multiplayer)
-    } else {
-        QuickPlayArguments::None
-    };
-
-    let launch_args = LaunchArgs::builder()
-        .auth(LaunchArgsAuth {
-            username: arg.username.clone(),
-            uuid: arg.uuid.clone(),
-            client_id: arg.client_id.clone(),
-            access_token: arg.access_token.clone(),
-        })
-        .game_dir(arg.game_directory.clone().unwrap())
-        .window_size((arg.width.unwrap(), arg.height.unwrap()))
-        .quick_play(quick_play_arguments)
-        .combine();
-
-    LaunchBuilder::builder()
-        .args(MinecraftArgs::Declared(launch_args))
-        .launch()
-        .await;
-}
+// async fn handle_launch(arg: &LaucnhArgs) {
+//     LaunchBuilder::builder()
+//         .args(launch_args)
+//         .launch(arg.jre)
+//         .await;
+// }
