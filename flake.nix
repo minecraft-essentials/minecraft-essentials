2554be98764fcaeb1a4aafc345a4bed58f34f7ab
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-14 08:24:30 +1100

    fix: flake for linux

--- flake.nix
+++ flake.nix
@@ -1,60 +1,60 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    agenix-shell.url = "github:aciceri/agenix-shell";
-  };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		flake-parts.url = "github:hercules-ci/flake-parts";
+		agenix-shell.url = "github:aciceri/agenix-shell";
+	};
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      flake-parts,
-      agenix-shell,
-      ...
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
+	outputs =
+		inputs@{
+			self,
+			nixpkgs,
+			flake-parts,
+			agenix-shell,
+			...
+		}:
+	flake-parts.lib.mkFlake { inherit inputs; } {
+		systems = nixpkgs.lib.systems.flakeExposed;
 
-      imports = [
-        # agenix-shell.flakeModules.default
-      ];
+		imports = [
+# agenix-shell.flakeModules.default
+		];
 
-      # agenix-shell = {
-      #   secrets = {
-      #     foo.file = ./secrets/foo.age;
-      #   };
-      # };
+# agenix-shell = {
+#   secrets = {
+#     foo.file = ./secrets/foo.age;
+#   };
+# };
 
-      perSystem =
-        {
-          pkgs,
-          config,
-          lib,
-          ...
-        }:
-        let
-          inherit (pkgs.darwin.apple_sdk.frameworks) SystemConfiguration;
-          toolchain = pkgs.rustPlatform;
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              (with toolchain;
-              [
-                rustc 
-                cargo
-              ])
-              clippy
-              rustfmt
-              rust-analyzer-unwrapped
-              darwin.libobjc
-              libiconv
-            ] ++ lib.optionals stdenv.isDarwin [
-              SystemConfiguration
-            ];
-            RUST_SRC_PATH = "${toolchain.rustLibSrc}";
-          };
-        };
-    };
+		perSystem =
+		{
+			pkgs,
+			config,
+			lib,
+			...
+		}:
+		let
+			inherit (pkgs.darwin.apple_sdk.frameworks) SystemConfiguration;
+		toolchain = pkgs.rustPlatform;
+		in
+		{
+			devShells.default = pkgs.mkShell {
+				packages = with pkgs; [
+					(with toolchain;
+					[
+					 rustc 
+					 cargo
+					])
+					clippy
+					rustfmt
+					rust-analyzer-unwrapped
+					libiconv
+				] ++ lib.optionals stdenv.isDarwin [
+				darwin.libobjc  
+				SystemConfiguration
+				];
+				RUST_SRC_PATH = "${toolchain.rustLibSrc}";
+			};
+		};
+	};
 }
